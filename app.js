52c2cb66c402d4aeed352c1188408e99b7d79604
leeyuan <dbdks32123@gmail.com>
2023-10-13 12:51:37 +0900

    index.html 불러오기 위해 readFile 함수 선언

--- app.js
+++ app.js
@@ -11,4 +11,12 @@ http.createServer(function(req, res){
 
   res.writeHead(200, writeHeadObject)
 
+  fs.readFile("./index.html", function(err, data){
+    if(err){
+      console.error("파일을 읽지 못했습니다.")
+    } else {
+      response.end(data)
+    }
+  })
+
 }).listen(8080);
