f31db05f754133af5f7bdc2d51d6c4372d442e0e
leeyuan <dbdks32123@gmail.com>
2023-10-13 13:01:38 +0900

    chartsArr에 적용될 중첩된 for문 작성

--- charts-package-model.js
+++ charts-package-model.js
@@ -3,9 +3,11 @@ const { listCharts } = require('billboard-top-100');
 listCharts((err, charts) => {
   console.log(charts);
   var chartsArr = [];
-  
-  for (let i = 0; i < charts.length; i++){
 
+  for (let i = 0; i < charts.length; i++){
+    for (let j = chartsArr; j <= chartsArr.length; j++){
+      
+    }
   }
 
 });
