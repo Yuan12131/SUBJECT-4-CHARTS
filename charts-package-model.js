52c0ecac3bd02f7bd30273babe20c84d86a3f7fa
leeyuan <dbdks32123@gmail.com>
2023-10-13 14:40:25 +0900

    chartArray 원소에 div태그를 붙인 데이터가 담긴index.html 파일 생성

--- charts-package-model.js
+++ charts-package-model.js
@@ -1,14 +1,19 @@
+const fs = require('fs');
+
 const { listCharts } = require('billboard-top-100');
 
 listCharts((err, charts) => {
-  console.log(charts);
-  const chartsArr = [];
+  let chartArray = [];
 
   for (let i = 0; i < charts.length; i++){
-    for (let j = chartsArr; j <= chartsArr.length; j++){
-      chartsArr[j] = charts[i].name + charts[i].url
-      break;
-    }
-  }
+    for (let j = chartArray.length; j <= chartArray.length; j++){
+        chartArray[j] = "<div>" + charts[i].url +">" + charts[i].name + "</div>";
 
-});
+        fs.writeFile('index.html', chartArray.join(''), function() {
+          console.log('File created');
+        });
+        
+        break
+      }
+    }
+  });
