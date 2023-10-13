bd3a1d962ac1067f5ba5afe27d97403e7eef2239
leeyuan <dbdks32123@gmail.com>
2023-10-13 18:14:36 +0900

    주석 추가

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http'); // http 불러오기
+const fs = require('fs'); // fs 불러오기
 
+// 서버 구축
 http.createServer(function(req, res){
   console.log(req.method)
   console.log(req.url)
@@ -11,6 +12,7 @@ http.createServer(function(req, res){
 
   res.writeHead(200, writeHeadObject)
 
+  // index.html 읽기
   fs.readFile("./index.html", function(err, data){
     if(err){
       console.error("파일을 읽지 못했습니다.")
@@ -19,4 +21,4 @@ http.createServer(function(req, res){
     }
   })
 
-}).listen(8080);
+}).listen(8080); // 포트번호 8080
