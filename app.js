de0a323f708e6b9a86c99479b749bb90d92ce6ef
leeyuan <dbdks32123@gmail.com>
2023-10-13 12:49:55 +0900

    서버 생성 함수 선언

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const http = require('http');
+
+http.createServer(function(req, res){
+  console.log(req.method)
+  console.log(req.url)
+
+  let writeHeadObject = {
+    'Content-Type' : 'text-html'
+  }
+
+  res.writeHead(200, writeHeadObject)
+
+})
