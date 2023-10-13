06671845296622c04e15e68c9f64e985ae413e3e
leeyuan <dbdks32123@gmail.com>
2023-10-13 13:00:37 +0900

    새로운 배열에 담기 위한 원래 배열 charts에 적용될 for문 작성

--- charts-package-model.js
+++ charts-package-model.js
@@ -3,4 +3,9 @@ const { listCharts } = require('billboard-top-100');
 listCharts((err, charts) => {
   console.log(charts);
   var chartsArr = [];
+  
+  for (let i = 0; i < charts.length; i++){
+
+  }
+
 });
