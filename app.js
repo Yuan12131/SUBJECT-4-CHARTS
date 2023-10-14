ebfa8da51886b9043e5ab3fd04355f47a8df950e
leeyuan <dbdks32123@gmail.com>
2023-10-14 16:24:28 +0900

    서버 응답 방식 추가

--- app.js
+++ app.js
@@ -3,22 +3,15 @@ const fs = require('fs'); // fs 불러오기
 
 // 서버 구축
 http.createServer(function(req, res){
-  console.log(req.method)
-  console.log(req.url)
-
-  let writeHeadObject = {
-    'Content-Type' : 'text-html'
-  }
-
-  res.writeHead(200, writeHeadObject)
-
   // index.html 읽기
-  fs.readFile("./index.html", function(err, data){
-    if(err){
-      console.error("파일을 읽지 못했습니다.")
-    } else {
-      res.end(data)
-    }
-  })
-
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, {'Content-Type' : 'text-html'})
+    fs.readFile("./index.html", function(err, data){
+      if(err){
+        console.error("파일을 읽지 못했습니다.")
+      } else {
+        res.end(data)
+      }
+    })
+  }
 }).listen(8080); // 포트번호 8080
